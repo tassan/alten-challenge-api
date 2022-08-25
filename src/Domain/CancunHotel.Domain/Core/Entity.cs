acb1c351a8ceeefc1c0d95a784e0d23708f3475a
Flávio Tassan <ftassan@outlook.com>
2022-08-25 10:44:22 -0300

    Add Entity model

---
+++ src/Domain/CancunHotel.Domain/Core/Entity.cs
@@ -0,0 +1,16 @@
+﻿using CancunHotel.Domain.Interfaces;
+
+namespace CancunHotel.Domain.Core;
+
+public abstract class Entity : IEntity
+{
+    public Guid Id { get; set; }
+    public DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset UpdatedAt { get; set; }
+    public bool Deleted { get; set; }
+
+    protected Entity()
+    {
+        Id = Guid.NewGuid();
+    }
+}
