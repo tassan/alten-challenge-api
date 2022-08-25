685fa810904a49bd6e649588e4f6f06b822196fc
Flávio Tassan <ftassan@outlook.com>
2022-08-25 10:44:11 -0300

    Add IEntity interface

---
+++ src/Domain/CancunHotel.Domain/Interfaces/IEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace CancunHotel.Domain.Interfaces;
+
+public interface IEntity
+{
+    public Guid Id { get; set; }
+    public DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset UpdatedAt { get; set; }
+    public bool Deleted { get; set; }
+}
