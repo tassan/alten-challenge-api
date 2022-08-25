d70c6c02706f622172031a5ea57a4484aeb30f97
Flávio Tassan <ftassan@outlook.com>
2022-08-25 14:34:32 -0300

    Add Customer.cs entity

---
+++ src/Domain/CancunHotel.Domain/Entities/Customer.cs
@@ -0,0 +1,10 @@
+﻿using CancunHotel.Domain.Core;
+
+namespace CancunHotel.Domain.Entities;
+
+public class Customer : Entity
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+}
