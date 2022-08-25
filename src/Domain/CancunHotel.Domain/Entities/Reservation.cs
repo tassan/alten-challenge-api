2aae478eaaa3d12caaaaca429bdb7685a72ab153
Flávio Tassan <ftassan@outlook.com>
2022-08-25 14:34:56 -0300

    Add Reservation.cs entity

---
+++ src/Domain/CancunHotel.Domain/Entities/Reservation.cs
@@ -0,0 +1,14 @@
+﻿using CancunHotel.Domain.Core;
+
+namespace CancunHotel.Domain.Entities;
+
+public class Reservation : Entity
+{
+    public DateTimeOffset CheckInDate { get; set; }
+    public DateTimeOffset CheckOutDate { get; set; }
+    public Customer Customer { get; set; }
+    public int AdultsAmount { get; set; }
+    public int ChildrenAmount { get; set; }
+    public int RoomAmount { get; set; }
+    public ICollection<Room> Rooms { get; set; }
+}
