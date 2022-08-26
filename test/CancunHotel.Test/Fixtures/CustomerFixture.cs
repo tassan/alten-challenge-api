9f9d35fcf6ca92cce5e1fc966f106c73e19f0412
Flávio Tassan <ftassan@outlook.com>
2022-08-26 11:39:19 -0300

    - Implement IDisposable interface - Change CreateCustomer to static function

--- test/CancunHotel.Test/Fixtures/CustomerFixture.cs
+++ test/CancunHotel.Test/Fixtures/CustomerFixture.cs
@@ -1,16 +1,22 @@
-﻿using Bogus;
+﻿using System;
+using Bogus;
 using CancunHotel.Domain.Entities;
 
 namespace CancunHotel.Test.Fixtures;
 
-public class CustomerFixture
+public class CustomerFixture : IDisposable
 {
-    public Customer CreateCustomer()
+    public static Customer CreateCustomer()
     {
         return new Faker<Customer>()
             .RuleFor(c => c.FirstName, f => f.Person.FirstName)
             .RuleFor(c => c.LastName, f => f.Person.LastName)
             .RuleFor(c => c.Email, f => f.Person.Email)
             .Generate();
+    }
+
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
     }
 }
