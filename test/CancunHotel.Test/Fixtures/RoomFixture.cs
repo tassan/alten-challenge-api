eefc1dcbb316ea02e44ba2e8d0e184c0d96c7b22
Flávio Tassan <ftassan@outlook.com>
2022-08-26 09:52:53 -0300

    Add RoomTest.cs and RoomFixture.cs

---
+++ test/CancunHotel.Test/Fixtures/RoomFixture.cs
@@ -0,0 +1,14 @@
+﻿using Bogus;
+using CancunHotel.Domain.Entities;
+
+namespace CancunHotel.Test.Fixtures;
+
+public class RoomFixture
+{
+    public Room CreateRoom()
+    {
+        return new Faker<Room>()
+            .RuleFor(r => r.BedsAmount, f => f.Random.Int(1, 4))
+            .Generate();
+    }
+}
