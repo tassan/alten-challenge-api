9f597c2f3d3ec00f2eeaa9679052e6dbc703287a
Flávio Tassan <ftassan@outlook.com>
2022-08-26 11:44:52 -0300

    - Implement IDisposable interface - Change CreateRoom to static function

--- test/CancunHotel.Test/Fixtures/RoomFixture.cs
+++ test/CancunHotel.Test/Fixtures/RoomFixture.cs
@@ -1,14 +1,20 @@
-﻿using Bogus;
+﻿using System;
+using Bogus;
 using CancunHotel.Domain.Entities;
 
 namespace CancunHotel.Test.Fixtures;
 
-public class RoomFixture
+public class RoomFixture : IDisposable
 {
-    public Room CreateRoom()
+    public static Room CreateRoom()
     {
         return new Faker<Room>()
             .RuleFor(r => r.BedsAmount, f => f.Random.Int(1, 4))
             .Generate();
+    }
+
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
     }
 }
