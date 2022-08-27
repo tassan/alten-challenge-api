128d49b8a336453aa8330328fbea96ee52a19c5d
Flávio Tassan <ftassan@outlook.com>
2022-08-27 15:14:02 -0300

    Add ReservationTest.cs

---
+++ test/CancunHotel.Test/Entities/ReservationTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using CancunHotel.Domain.Entities;
+using CancunHotel.Test.Fixtures;
+using FluentAssertions;
+using Xunit;
+
+namespace CancunHotel.Test.Entities;
+
+public class ReservationTest
+{
+    [Fact]
+    public void Should_Create_Valid_Reservation()
+    {
+        var reservation = ReservationFixture.CreateValidReservation();
+
+        reservation.CheckInDate
+            .Should()
+            .Be(reservation.CheckInDate);
+        
+        reservation.CheckOutDate
+            .Should()
+            .Be(reservation.CheckOutDate);
+
+        reservation.GuestsAmount
+            .Should()
+            .BeGreaterThanOrEqualTo(1);
+        
+        reservation.Customer
+            .Should()
+            .NotBeNull();
+    }
+    
+    [Fact]
+    public void Should_Create_Valid_Reservation_With_Custom_Customer()
+    {
+        var customer = CustomerFixture.CreateCustomer();
+        
+        var reservation = new Reservation(customer)
+        {
+            CheckInDate = DateTimeOffset.UtcNow,
+            CheckOutDate = DateTimeOffset.UtcNow.AddDays(3),
+            GuestsAmount = 4
+        };
+
+        reservation.CheckInDate
+            .Should()
+            .Be(reservation.CheckInDate);
+        
+        reservation.CheckOutDate
+            .Should()
+            .Be(reservation.CheckOutDate);
+
+        reservation.GuestsAmount
+            .Should()
+            .BeGreaterThan(1);
+        
+        reservation.Customer
+            .Should()
+            .NotBeNull();
+    }
+}
