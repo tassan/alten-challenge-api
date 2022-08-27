441136979d0e7421e581a1ab6065d09de334c197
Flávio Tassan <ftassan@outlook.com>
2022-08-27 19:17:26 -0300

    Ad ICustomerRepository.cs

---
+++ src/Domain/CancunHotel.Domain/Interfaces/Repository/ICustomerRepository.cs
@@ -0,0 +1,15 @@
+﻿using CancunHotel.Domain.Entities;
+using CancunHotel.Domain.Interfaces.Data;
+
+namespace CancunHotel.Domain.Interfaces.Repository;
+
+public interface ICustomerRepository : IRepository
+{
+    Task<Customer> GetById(Guid id);
+    Task<Customer> GetByEmail(string email);
+    Task<IEnumerable<Customer>> GetAll();
+
+    void Add(Customer customer);
+    void Update(Customer customer);
+    void Remove(Customer customer);
+}
