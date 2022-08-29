04ce6054bb515979ed3e019bd1695a24b2b0007c
Flávio Tassan <ftassan@outlook.com>
2022-08-29 20:55:40 -0300

    Add CustomerViewModel.cs

---
+++ src/Application/CancunHotel.Application/ViewModels/CustomerViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CancunHotel.Application.ViewModels;
+
+public class CustomerViewModel
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+}
