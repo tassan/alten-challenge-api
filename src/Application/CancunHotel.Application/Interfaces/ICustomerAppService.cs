f574e2b8278588071f21c6dfa74d9fe191aa9477
Flávio Tassan <ftassan@outlook.com>
2022-08-29 20:54:41 -0300

    Add ICustomerAppService.cs

---
+++ src/Application/CancunHotel.Application/Interfaces/ICustomerAppService.cs
@@ -0,0 +1,13 @@
+﻿using CancunHotel.Application.ViewModels;
+using FluentValidation.Results;
+
+namespace CancunHotel.Application.Interfaces;
+
+public interface ICustomerAppService : IDisposable
+{
+    Task<IEnumerable<CustomerViewModel>> GetAll();
+    Task<CustomerViewModel> GetById(Guid id);
+    Task<ValidationResult> Register(CustomerViewModel customerViewModel);
+    Task<ValidationResult> Update(CustomerViewModel customerViewModel);
+    Task<ValidationResult> Remove(Guid id);
+}
