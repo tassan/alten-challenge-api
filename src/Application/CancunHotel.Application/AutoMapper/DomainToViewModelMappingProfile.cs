1ec1eaf08ab371bcd46e44a44bc2ffc5997948b5
Flávio Tassan <ftassan@outlook.com>
2022-08-29 20:55:59 -0300

    Add AutoMapper profile configuration

---
+++ src/Application/CancunHotel.Application/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using CancunHotel.Application.ViewModels;
+using CancunHotel.Domain.Entities;
+
+namespace CancunHotel.Application.AutoMapper
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        public DomainToViewModelMappingProfile()
+        {
+            CreateMap<Customer, CustomerViewModel>();
+        }
+    }
+}
---
+++ src/Application/CancunHotel.Application/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CancunHotel.Application.ViewModels;
+using CancunHotel.Domain.Entities;
+
+namespace CancunHotel.Application.AutoMapper
+{
+    public class ViewModelToDomainMappingProfile : Profile
+    {
+        public ViewModelToDomainMappingProfile()
+        {
+            CreateMap<CustomerViewModel, Customer>()
+                .ConstructUsing(c => new Customer(c.FirstName, c.Email, c.Email));
+        }
+    }
+}
