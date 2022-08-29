48329026793da29c4cad5271cc25857f61c36c8e
Flávio Tassan <ftassan@outlook.com>
2022-08-29 20:51:20 -0300

    Change nullable properties to non-nullable

--- src/Domain/CancunHotel.Domain/Entities/Customer.cs
+++ src/Domain/CancunHotel.Domain/Entities/Customer.cs
@@ -4,13 +4,13 @@ namespace CancunHotel.Domain.Entities;
 
 public class Customer : Entity
 {
-    public string? FirstName { get; set; }
-    public string? LastName { get; set; }
-    public string? Email { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
 
     public Customer() { }
 
-    public Customer(string? firstName, string? lastName, string? email)
+    public Customer(string firstName, string lastName, string email)
     {
         FirstName = firstName;
         LastName = lastName;
