bf64950e9f7d16208c513d8e7388ad1a780938b3
Flávio Tassan <ftassan@outlook.com>
2022-08-29 20:56:21 -0300

    Add CreateCustomerViewModel to fixture

--- tests/CancunHotel.Tests/Fixtures/CustomerFixture.cs
+++ tests/CancunHotel.Tests/Fixtures/CustomerFixture.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Bogus;
+using CancunHotel.Application.ViewModels;
 using CancunHotel.Domain.Entities;
 
 namespace CancunHotel.Tests.Fixtures;
@@ -9,6 +10,15 @@ public class CustomerFixture : IDisposable
     public static Customer CreateCustomer()
     {
         return new Faker<Customer>()
+            .RuleFor(c => c.FirstName, f => f.Person.FirstName)
+            .RuleFor(c => c.LastName, f => f.Person.LastName)
+            .RuleFor(c => c.Email, f => f.Person.Email)
+            .Generate();
+    }
+    
+    public static CustomerViewModel CreateCustomerViewModel()
+    {
+        return new Faker<CustomerViewModel>()
             .RuleFor(c => c.FirstName, f => f.Person.FirstName)
             .RuleFor(c => c.LastName, f => f.Person.LastName)
             .RuleFor(c => c.Email, f => f.Person.Email)
