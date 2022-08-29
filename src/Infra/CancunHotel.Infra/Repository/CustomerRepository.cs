7c84c1420103e58635e69e1967074c631cda229f
Flávio Tassan <ftassan@outlook.com>
2022-08-29 20:57:58 -0300

    Add CustomerRepository.cs

--- src/Infra/CancunHotel.Infra/Repository/CustomerRepository.cs
+++ src/Infra/CancunHotel.Infra/Repository/CustomerRepository.cs
@@ -1,44 +1,55 @@
 ﻿using CancunHotel.Domain.Entities;
 using CancunHotel.Domain.Interfaces.Data;
 using CancunHotel.Domain.Interfaces.Repository;
+using CancunHotel.Infra.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace CancunHotel.Infra.Repository;
 
 public class CustomerRepository : ICustomerRepository
 {
-    public void Dispose()
+    private readonly ApplicationContext _context;
+    private readonly DbSet<Customer> _dbSet;
+
+    public CustomerRepository(ApplicationContext context)
     {
-        throw new NotImplementedException();
+        _context = context;
+        _dbSet = _context.Set<Customer>();
     }
 
-    public IUnitOfWork UnitOfWork { get; }
-    public Task<Customer> GetById(Guid id)
+    public IUnitOfWork UnitOfWork => _context;
+    public async Task<Customer> GetById(Guid id)
     {
-        throw new NotImplementedException();
+        return await _dbSet.FindAsync(id);
     }
 
-    public Task<Customer> GetByEmail(string email)
+    public async Task<Customer> GetByEmail(string email)
     {
-        throw new NotImplementedException();
+        return await _dbSet.AsNoTracking().FirstOrDefaultAsync(c => c.Email == email);
     }
 
-    public Task<IEnumerable<Customer>> GetAll()
+    public async Task<IEnumerable<Customer>> GetAll()
     {
-        throw new NotImplementedException();
+        return await _dbSet.ToListAsync();
     }
 
     public void Add(Customer customer)
     {
-        throw new NotImplementedException();
+        _context.Add(customer);
+        _context.SaveChanges();
     }
 
     public void Update(Customer customer)
     {
-        throw new NotImplementedException();
+        _context.Update(customer);
+        _context.SaveChanges();
     }
 
     public void Remove(Customer customer)
     {
-        throw new NotImplementedException();
+        _context.Remove(customer);
+        _context.SaveChanges();
     }
+    
+    public void Dispose() => _context.Dispose();
 }
