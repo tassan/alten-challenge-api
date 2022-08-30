badbba278273d20e043c95f37bfd67256faec111
Flávio Tassan <ftassan@outlook.com>
2022-08-30 17:39:02 -0300

    Add UpdateBookingViewModel.cs

---
+++ src/Application/CancunHotel.Application/ViewModels/UpdateBookingViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CancunHotel.Application.ViewModels;
+
+public class UpdateBookingViewModel
+{
+    public Guid Id { get; set; }
+    public Guid CustomerId { get; set; }
+    public DateTime CheckInDate { get; set; }
+    public DateTime CheckOutDate { get; set; }
+    public int GuestsAmount { get; set; }
+}
