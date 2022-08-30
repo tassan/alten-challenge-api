790b0b6add148ec25cb618c7376dfb1627d824aa
Flávio Tassan <ftassan@outlook.com>
2022-08-30 13:43:14 -0300

    Add ReadBookingViewModel.cs

---
+++ src/Application/CancunHotel.Application/ViewModels/ReadBookingViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CancunHotel.Application.ViewModels;
+
+public class ReadBookingViewModel
+{
+    public Guid CustomerId { get; set; }
+    public DateTime CheckInDate { get; set; }
+    public DateTime CheckOutDate { get; set; }
+    public int GuestsAmount { get; set; }
+    public CustomerViewModel Customer { get; set; }
+}
