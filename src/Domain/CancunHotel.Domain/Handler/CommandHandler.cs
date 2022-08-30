03e5f083b057b4844aa165cd45835c4b0e564113
Flávio Tassan <ftassan@outlook.com>
2022-08-29 23:36:40 -0300

    Add CommandHandler.cs to handle unit of work commands

---
+++ src/Domain/CancunHotel.Domain/Handler/CommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using CancunHotel.Domain.Interfaces.Data;
+using FluentValidation.Results;
+
+namespace CancunHotel.Domain.Handler;
+
+public class CommandHandler
+{
+    protected readonly ValidationResult ValidationResult;
+
+    protected CommandHandler() => ValidationResult = new ValidationResult();
+
+    protected void AddError(string message) =>
+        ValidationResult.Errors.Add(new ValidationFailure(string.Empty, message));
+
+    protected async Task<ValidationResult> Commit(
+        IUnitOfWork uow,
+        string message)
+    {
+        if (!await uow.CommitAsync())
+            AddError(message);
+        return ValidationResult;
+    }
+
+    protected async Task<ValidationResult> Commit(IUnitOfWork uow) =>
+        await Commit(uow, "There was an error saving data").ConfigureAwait(false);
+}
