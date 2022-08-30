ebbbbf3c47b856ea0cc460484dd12c6cd112c509
Flávio Tassan <ftassan@outlook.com>
2022-08-29 21:34:32 -0300

    Add BirthDate property

--- src/Domain/CancunHotel.Domain/Entities/Customer.cs
+++ src/Domain/CancunHotel.Domain/Entities/Customer.cs
@@ -7,6 +7,7 @@ public class Customer : Entity
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Email { get; set; }
+    public DateTime BirthDate { get; set; }
 
     public Customer() { }
 
