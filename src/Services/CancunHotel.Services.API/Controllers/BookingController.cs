2c360decda38293513a96e41509e8893298b9334
Flávio Tassan <ftassan@outlook.com>
2022-08-30 02:33:48 -0300

    Add BookingController.cs

---
+++ src/Services/CancunHotel.Services.API/Controllers/BookingController.cs
@@ -0,0 +1,31 @@
+﻿using CancunHotel.Application.Interfaces;
+using CancunHotel.Application.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CancunHotel.Services.API.Controllers;
+
+[ApiController]
+public class BookingController : ControllerBase
+{
+    private readonly IBookingService _bookingService;
+
+    public BookingController(IBookingService bookingService)
+    {
+        _bookingService = bookingService;
+    }
+
+    [AllowAnonymous]
+    [HttpPost("booking-management")]
+    public async Task<IActionResult> Post([FromBody] BookingViewModel bookingViewModel)
+    {
+        return !ModelState.IsValid ? BadRequest(ModelState) : Ok(await _bookingService.Register(bookingViewModel));
+    }
+    
+    [AllowAnonymous]
+    [HttpGet("booking-management")]
+    public async Task<IActionResult> GetAll()
+    {
+        return !ModelState.IsValid ? BadRequest(ModelState) : Ok(await _bookingService.GetAll());
+    }
+}
