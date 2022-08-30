a1d96d4abe082832b17afc625757bbbec56a0025
Flávio Tassan <ftassan@outlook.com>
2022-08-29 21:09:10 -0300

    Add CustomerAppServiceTests.cs

---
+++ tests/CancunHotel.Tests/Services/CustomerAppServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using CancunHotel.Application.Interfaces;
+using CancunHotel.Domain.Entities;
+using CancunHotel.Domain.Interfaces.Repository;
+using CancunHotel.Tests.Fixtures;
+using FakeItEasy;
+using Xunit;
+
+namespace CancunHotel.Tests.Services;
+
+public class CustomerAppServiceTests
+{
+    [Fact]
+    public void Should_Register_Customer()
+    {
+        var customerRepository = A.Fake<ICustomerRepository>();
+        var customerService = A.Fake<ICustomerAppService>();
+        var customerViewModel = CustomerFixture.CreateCustomerViewModel();
+        
+        customerService.Register(customerViewModel);
+
+        A.CallTo(() => customerRepository.Add(new Customer()))
+            .MustHaveHappened();
+    }
+}
