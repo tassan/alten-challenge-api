3db81d19bb185923689501e604558613fc1b00dc
Flávio Tassan <ftassan@outlook.com>
2022-08-29 21:08:10 -0300

    Add EntityService.cs and IEntityService.cs

---
+++ src/Domain/CancunHotel.Domain/DomainObjects/EntityService.cs
@@ -0,0 +1,31 @@
+﻿using CancunHotel.Domain.Interfaces;
+
+namespace CancunHotel.Domain.DomainObjects;
+
+public abstract class EntityService : IEntityService<Entity>
+{
+    public Task<Entity> Get(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Entity>> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Add(Entity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Update(Entity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Delete(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Domain/CancunHotel.Domain/Interfaces/IEntityService.cs
@@ -0,0 +1,10 @@
+﻿namespace CancunHotel.Domain.Interfaces;
+
+public interface IEntityService<TEntity> where TEntity : IEntity
+{
+    Task<TEntity> Get(Guid id);
+    Task<IEnumerable<TEntity>> GetAll();
+    Task Add(TEntity entity);
+    Task Update(TEntity entity);
+    Task Delete(Guid id);
+}
