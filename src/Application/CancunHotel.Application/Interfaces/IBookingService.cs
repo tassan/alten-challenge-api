c7cec9c58601b3af671739e1eb3f047ac8daf06a
Flávio Tassan <ftassan@outlook.com>
2022-08-30 01:58:42 -0300

    - Implement Register booking in BookingService.cs - Implement CheckReservationAvailability in BookingService.cs - Add CheckReservationAvailability to IBookingService.cs

--- src/Application/CancunHotel.Application/Interfaces/IBookingService.cs
+++ src/Application/CancunHotel.Application/Interfaces/IBookingService.cs
@@ -1,9 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
-using CancunHotel.Application.ViewModels;
+﻿using CancunHotel.Application.ViewModels;
+using FluentValidation.Results;
 
 namespace CancunHotel.Application.Interfaces;
 
 public interface IBookingService : IDisposable
 {
     Task<ValidationResult> Register(BookingViewModel bookingViewModel);
+    bool CheckReservationAvailability(DateTime checkIn, DateTime checkOut);
 }
--- src/Application/CancunHotel.Application/Services/BookingService.cs
+++ src/Application/CancunHotel.Application/Services/BookingService.cs
@@ -1,16 +1,59 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using AutoMapper;
 using CancunHotel.Application.Interfaces;
 using CancunHotel.Application.ViewModels;
+using CancunHotel.Domain.Entities;
+using CancunHotel.Domain.Handler;
+using CancunHotel.Domain.Interfaces.Repository;
+using CancunHotel.Domain.Validations;
+using FluentValidation.Results;
 
 namespace CancunHotel.Application.Services;
 
-public class BookingService : IBookingService
+public class BookingService : CommandHandler, IBookingService
 {
-    public Task<ValidationResult> Register(BookingViewModel bookingViewModel)
+    private readonly IReservationRepository _reservationRepository;
+    private readonly ICustomerRepository _customerRepository;
+    private readonly IMapper _mapper;
+
+    protected BookingService(IReservationRepository reservationRepository, ICustomerRepository customerRepository,
+        IMapper mapper)
     {
-        throw new NotImplementedException();
+        _reservationRepository = reservationRepository;
+        _customerRepository = customerRepository;
+        _mapper = mapper;
     }
-    
+
+    public async Task<ValidationResult> Register(BookingViewModel bookingViewModel)
+    {
+        var reservation = _mapper.Map<Reservation>(bookingViewModel);
+        reservation.Customer = await _customerRepository.GetById(reservation.CustomerId);
+
+        var validationResult = await ValidateBooking(reservation);
+
+        if (!validationResult.IsValid)
+            return validationResult;
+
+        if (CheckReservationAvailability(reservation.CheckInDate, reservation.CheckOutDate))
+        {
+            AddError("There's already one reservation for the desired date");
+            return ValidationResult;
+        }
+        
+        _reservationRepository.Add(reservation);
+        return await Commit(_reservationRepository.UnitOfWork);
+    }
+
+    public bool CheckReservationAvailability(DateTime checkIn, DateTime checkOut)
+    {
+        return _reservationRepository.GetByDates(checkIn, checkOut).Any();
+    }
+
+    private async Task<ValidationResult> ValidateBooking(Reservation reservation)
+    {
+        var bookingValidation = new BookingValidation();
+        return await bookingValidation.ValidateAsync(reservation);
+    }
+
     public void Dispose()
     {
         GC.SuppressFinalize(this);
