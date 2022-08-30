a0d716eede2a6cecb01f33d19990ac35296d071f
Flávio Tassan <ftassan@outlook.com>
2022-08-30 00:36:17 -0300

    Add ReservationRepository.cs

--- src/Infra/CancunHotel.Infra/Repository/ReservationRepository.cs
+++ src/Infra/CancunHotel.Infra/Repository/ReservationRepository.cs
@@ -1,52 +1,21 @@
 ﻿using CancunHotel.Domain.Entities;
-using CancunHotel.Domain.Interfaces.Data;
 using CancunHotel.Domain.Interfaces.Repository;
 using CancunHotel.Infra.Context;
 using Microsoft.EntityFrameworkCore;
 
 namespace CancunHotel.Infra.Repository;
 
-public class ReservationRepository : IReservationRepository
+public class ReservationRepository : GenericRepository<Reservation, ApplicationContext>, IReservationRepository
 {
-    private readonly ApplicationContext _context;
-    private readonly DbSet<Reservation> _dbSet;
-
-    public ReservationRepository(ApplicationContext context)
+    public ReservationRepository(ApplicationContext context) : base(context)
     {
-        _context = context;
-        _dbSet = _context.Set<Reservation>();
-    }
-
-    public IUnitOfWork UnitOfWork => _context;
-    public async Task<Reservation> GetById(Guid id)
-    {
-        return await _dbSet.FindAsync(id);
     }
 
     public async Task<Reservation> GetByCustomer(Guid customerId)
     {
-        return await _dbSet.AsNoTracking().FirstOrDefaultAsync(r => r.CustomerId == customerId);
+#pragma warning disable CS8603
+        return await DbSet.AsNoTracking()
+            .FirstOrDefaultAsync(r => r.CustomerId == customerId);
+#pragma warning restore CS8603
     }
-
-    public async Task<IEnumerable<Reservation>> GetAll()
-    {
-        return await _dbSet.ToListAsync();
-    }
-
-    public void Add(Reservation reservation)
-    {
-        _dbSet.Add(reservation);
-    }
-
-    public void Update(Reservation reservation)
-    {
-        _dbSet.Update(reservation);
-    }
-
-    public void Remove(Reservation reservation)
-    {
-        _dbSet.Update(reservation);
-    }
-    
-    public void Dispose() => _context.Dispose();
 }
