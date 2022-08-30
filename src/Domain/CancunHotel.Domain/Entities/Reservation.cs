2ee63ab7175d35d614e5216986b9d0cc2c7f4a86
Flávio Tassan <ftassan@outlook.com>
2022-08-30 02:31:44 -0300

    Fix datetime timekind

--- src/Domain/CancunHotel.Domain/Entities/Reservation.cs
+++ src/Domain/CancunHotel.Domain/Entities/Reservation.cs
@@ -10,7 +10,7 @@ public class Reservation : Entity
     public DateTime CheckOutDate { get; set; }
     public int GuestsAmount { get; set; }
 
-    [IgnoreDataMember] public int DaysReservedCount => (CheckOutDate - CheckInDate).Days;
+    public int DaysReservedCount => (CheckOutDate - CheckInDate).Days;
 
     public Customer Customer { get; set; }
 
@@ -23,8 +23,8 @@ public class Reservation : Entity
     public Reservation(Guid customerId, DateTime checkInDate, DateTime checkOutDate, int guestsAmount)
     {
         CustomerId = customerId;
-        CheckInDate = new DateTime(checkInDate.Year, checkInDate.Month, checkInDate.Day, 0, 0, 0);
-        CheckOutDate = new DateTime(checkOutDate.Year, checkOutDate.Month, checkOutDate.Day, 23, 59, 59);
+        CheckInDate = new DateTime(checkInDate.Year, checkInDate.Month, checkInDate.Day, 0, 0, 0, DateTimeKind.Utc);
+        CheckOutDate = new DateTime(checkOutDate.Year, checkOutDate.Month, checkOutDate.Day, 23, 59, 59, DateTimeKind.Utc);
         GuestsAmount = guestsAmount;
     }
 
