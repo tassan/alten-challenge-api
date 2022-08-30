5092728838abf6b7e2178e299b27e440f72e84c8
Flávio Tassan <ftassan@outlook.com>
2022-08-30 17:42:09 -0300

    Add GetAll, Update, Delete and CheckReservation to BookingController.cs

--- src/Services/CancunHotel.Services.API/Controllers/BookingController.cs
+++ src/Services/CancunHotel.Services.API/Controllers/BookingController.cs
@@ -16,6 +16,13 @@ public class BookingController : ControllerBase
     }
 
     [AllowAnonymous]
+    [HttpGet("booking-management")]
+    public async Task<IActionResult> GetAll()
+    {
+        return !ModelState.IsValid ? BadRequest(ModelState) : Ok(await _bookingService.GetAll());
+    }
+    
+    [AllowAnonymous]
     [HttpPost("booking-management")]
     public async Task<IActionResult> Post([FromBody] CreateBookingViewModel bookingViewModel)
     {
@@ -23,9 +30,38 @@ public class BookingController : ControllerBase
     }
     
     [AllowAnonymous]
-    [HttpGet("booking-management")]
-    public async Task<IActionResult> GetAll()
+    [HttpPut("booking-management")]
+    public async Task<IActionResult> Put([FromBody] UpdateBookingViewModel bookingViewModel)
     {
-        return !ModelState.IsValid ? BadRequest(ModelState) : Ok(await _bookingService.GetAll());
+        return !ModelState.IsValid ? BadRequest(ModelState) : Ok(await _bookingService.Update(bookingViewModel));
+    }
+    
+    [AllowAnonymous]
+    [HttpDelete("booking-management/{id:guid}")]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        return !ModelState.IsValid ? BadRequest(ModelState) : Ok(await _bookingService.Remove(id));
+    }
+
+    [AllowAnonymous]
+    [HttpGet("check-reservation/{email}")]
+    public async Task<IActionResult> CheckReservation(string email)
+    {
+        var reservation = await _bookingService.GetReservationByEmail(email);
+
+        if (reservation is null) return NotFound("We couldn't find any reservation for the entered user e-mail");
+
+        return Ok(reservation);
+    }
+
+    [AllowAnonymous]
+    [HttpPost("check-availability")]
+    public IActionResult CheckAvailability([FromQuery] DateTime checkIn, [FromQuery] DateTime checkOut)
+    {
+        var isAvailable = _bookingService.CheckReservationAvailability(checkIn, checkOut);
+
+        return Ok(isAvailable
+            ? $"There's already one reservation for the desired date of {checkIn} to {checkOut}"
+            : $"The desired date of {checkIn} to {checkOut} is available to reservation");
     }
 }
