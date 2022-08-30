12a448eabeeb14874104f2ba729a9baa6ba5a47d
Flávio Tassan <ftassan@outlook.com>
2022-08-29 21:07:24 -0300

    Add CustomerValidation.cs

---
+++ src/Domain/CancunHotel.Domain/Validations/CustomerValidation.cs
@@ -0,0 +1,34 @@
+﻿using CancunHotel.Domain.Entities;
+using FluentValidation;
+
+namespace CancunHotel.Domain.Validations;
+
+public class CustomerValidation : AbstractValidator<Customer>
+{
+    protected void ValidateFirstName()
+    {
+        RuleFor(c => c.FirstName)
+            .NotEmpty().WithMessage("Please ensure you have entered the Name")
+            .Length(2, 150).WithMessage("The Name must have between 2 and 150 characters");
+    }
+    
+    protected void ValidateLastName()
+    {
+        RuleFor(c => c.LastName)
+            .NotEmpty().WithMessage("Please ensure you have entered the Name")
+            .Length(2, 150).WithMessage("The Name must have between 2 and 150 characters");
+    }
+    
+    protected void ValidateEmail()
+    {
+        RuleFor(c => c.Email)
+            .NotEmpty()
+            .EmailAddress();
+    }
+    
+    protected void ValidateId()
+    {
+        RuleFor(c => c.Id)
+            .NotEqual(Guid.Empty);
+    }
+}
