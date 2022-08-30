b939b42ca81c895d9a5280004a4e29d355306563
Flávio Tassan <ftassan@outlook.com>
2022-08-30 17:41:10 -0300

    Add Update, Remove and GetReservationByEmail to IBookingService.cs

--- src/Application/CancunHotel.Application/Interfaces/IBookingService.cs
+++ src/Application/CancunHotel.Application/Interfaces/IBookingService.cs
@@ -6,6 +6,9 @@ namespace CancunHotel.Application.Interfaces;
 public interface IBookingService : IDisposable
 {
     Task<ValidationResult> Register(CreateBookingViewModel bookingViewModel);
+    Task<ReadBookingViewModel?> GetReservationByEmail(string email);
     bool CheckReservationAvailability(DateTime checkIn, DateTime checkOut);
     Task<IEnumerable<ReadBookingViewModel>> GetAll();
+    Task<ValidationResult> Update(UpdateBookingViewModel bookingViewModel);
+    Task<ValidationResult> Remove(Guid id);
 }
