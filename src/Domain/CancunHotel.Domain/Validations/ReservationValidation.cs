c4a2f525351263db900065ca036bb279bfd923ee
Flávio Tassan <ftassan@outlook.com>
2022-08-30 01:35:05 -0300

    Add ReservationValidation.cs

---
+++ src/Domain/CancunHotel.Domain/Validations/ReservationValidation.cs
@@ -0,0 +1,63 @@
+﻿using CancunHotel.Domain.Entities;
+using FluentValidation;
+
+namespace CancunHotel.Domain.Validations;
+
+public class ReservationValidation : AbstractValidator<Reservation>
+{
+    protected void ValidateCheckIn()
+    {
+        RuleFor(r => r.CheckInDate)
+            .NotEmpty()
+            .WithMessage("Please ensure you have entered the Check-In Date")
+            .Must(IsLessThan30Days)
+            .WithMessage("The Check-In can't start in more than 30 days");
+    }
+
+    protected void ValidateCheckOut()
+    {
+        RuleFor(r => r.CheckOutDate)
+            .NotEmpty()
+            .WithMessage("Please ensure you have entered the Check-Out Date")
+            .GreaterThanOrEqualTo(r => r.CheckInDate)
+            .WithMessage("The Check-Out date must be greater or equal to Check-In date");
+    }
+
+    protected void ValidateGuestsAmount()
+    {
+        RuleFor(r => r.GuestsAmount)
+            .NotEmpty()
+            .WithMessage("Please ensure you have entered the Guests Amount")
+            .GreaterThan(0).WithMessage("The Guests Amount must be greater than 0");
+    }
+
+    protected void ValidateReservationLength()
+    {
+        RuleFor(r => r.DaysReservedCount)
+            .GreaterThan(0)
+            .WithMessage("The reservation length must be greather than 0 Days")
+            .LessThanOrEqualTo(3)
+            .WithMessage("The reservation length must be less or equal to 3 Days");
+    }
+
+    protected void ValidateReservationCustomer()
+    {
+        RuleFor(r => r.CustomerId)
+            .NotEqual(Guid.Empty);
+
+        RuleFor(r => r.Customer.Deleted)
+            .NotEqual(true)
+            .WithMessage("The Customer must be active");
+    }
+
+    protected void ValidateId()
+    {
+        RuleFor(c => c.Id)
+            .NotEqual(Guid.Empty);
+    }
+    
+    protected static bool IsLessThan30Days(DateTime date)
+    {
+        return date <= DateTime.UtcNow.AddDays(30);
+    }
+}
