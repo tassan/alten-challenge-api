87fe488877e13e363e768832af3889fc62e82ea2
Flávio Tassan <ftassan@outlook.com>
2022-08-30 01:35:15 -0300

    Add BookingValidation.cs

---
+++ src/Domain/CancunHotel.Domain/Validations/BookingValidation.cs
@@ -0,0 +1,14 @@
+﻿namespace CancunHotel.Domain.Validations;
+
+public class BookingValidation : ReservationValidation
+{
+    public BookingValidation()
+    {
+        ValidateId();
+        ValidateCheckIn();
+        ValidateCheckOut();
+        ValidateReservationLength();
+        ValidateGuestsAmount();
+        ValidateReservationCustomer();
+    }
+}
