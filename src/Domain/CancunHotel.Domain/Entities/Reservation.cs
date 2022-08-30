6ac866311c9a54a87c838f64b03fa8d673f7ed55
Flávio Tassan <ftassan@outlook.com>
2022-08-30 11:50:42 -0300

    Change indentation of Reservation.cs

--- src/Domain/CancunHotel.Domain/Entities/Reservation.cs
+++ src/Domain/CancunHotel.Domain/Entities/Reservation.cs
@@ -24,7 +24,8 @@ public class Reservation : Entity
     {
         CustomerId = customerId;
         CheckInDate = new DateTime(checkInDate.Year, checkInDate.Month, checkInDate.Day, 0, 0, 0, DateTimeKind.Utc);
-        CheckOutDate = new DateTime(checkOutDate.Year, checkOutDate.Month, checkOutDate.Day, 23, 59, 59, DateTimeKind.Utc);
+        CheckOutDate = new DateTime(checkOutDate.Year, checkOutDate.Month, checkOutDate.Day, 23, 59, 59,
+            DateTimeKind.Utc);
         GuestsAmount = guestsAmount;
     }
 
