63b746900eef55db7cedce16113ae4f357813d59
Flávio Tassan <ftassan@outlook.com>
2022-08-29 21:50:57 -0300

    Add BirthDate field Migration

---
+++ src/Infra/CancunHotel.Infra/Migrations/20220830004936_Customer BirthDate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CancunHotel.Infra.Migrations
+{
+    public partial class CustomerBirthDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BirthDate",
+                table: "Customers",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BirthDate",
+                table: "Customers");
+        }
+    }
+}
