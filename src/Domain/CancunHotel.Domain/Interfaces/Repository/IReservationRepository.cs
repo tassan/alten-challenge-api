2f0e973b08f8b819e38771a7183eda89daeec413
Flávio Tassan <ftassan@outlook.com>
2022-08-30 00:11:44 -0300

    Add IReservationRepository.cs interface

---
+++ src/Domain/CancunHotel.Domain/Interfaces/Repository/IReservationRepository.cs
@@ -0,0 +1,15 @@
+﻿using CancunHotel.Domain.Entities;
+using CancunHotel.Domain.Interfaces.Data;
+
+namespace CancunHotel.Domain.Interfaces.Repository;
+
+public interface IReservationRepository : IRepository
+{
+    Task<Reservation> GetById(Guid id);
+    Task<Reservation> GetByCustomer(Guid customerId);
+    Task<IEnumerable<Reservation>> GetAll();
+
+    void Add(Reservation reservation);
+    void Update(Reservation reservation);
+    void Remove(Reservation reservation);
+}
