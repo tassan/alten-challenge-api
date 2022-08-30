28284a8377333e1e6fd22f47c6785a9bc798596a
Flávio Tassan <ftassan@outlook.com>
2022-08-29 21:00:49 -0300

    Add ApplicationContext.cs

---
+++ src/Infra/CancunHotel.Infra/Context/ApplicationContext.cs
@@ -0,0 +1,41 @@
+﻿using CancunHotel.Domain.DomainObjects;
+using CancunHotel.Domain.Entities;
+using CancunHotel.Domain.Interfaces.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CancunHotel.Infra.Context;
+
+public class ApplicationContext : DbContext, IUnitOfWork
+{
+    public DbSet<Customer> Customers { get; set; }
+
+    public ApplicationContext(DbContextOptions options)
+        : base(options)
+    {
+    }
+
+    public async Task<bool> Commit()
+    {
+        var success = await SaveChangesAsync() > 0;
+        return success;
+    }
+
+    public override int SaveChanges()
+    {
+        var entries = ChangeTracker
+            .Entries()
+            .Where(e => e.Entity is Entity && e.State is EntityState.Added or EntityState.Modified);
+
+        foreach (var entityEntry in entries)
+        {
+            ((Entity) entityEntry.Entity).UpdatedAt = DateTime.UtcNow;
+
+            if (entityEntry.State == EntityState.Added)
+            {
+                ((Entity) entityEntry.Entity).CreatedAt = DateTime.UtcNow;
+            }
+        }
+
+        return base.SaveChanges();
+    }
+}
