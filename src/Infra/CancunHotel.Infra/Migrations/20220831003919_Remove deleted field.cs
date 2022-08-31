a0559cded5cdf51ba11a5ee37dea51aff5ff7bbf
Flávio Tassan <ftassan@outlook.com>
2022-08-30 21:45:03 -0300

    Add Remove deleted field migration

---
+++ src/Infra/CancunHotel.Infra/Migrations/20220831003919_Remove deleted field.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CancunHotel.Infra.Migrations
+{
+    public partial class Removedeletedfield : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reservations_Customers_CustomerId",
+                table: "Reservations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reservations_CustomerId",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "Customers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "Reservations",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "Customers",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_CustomerId",
+                table: "Reservations",
+                column: "CustomerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reservations_Customers_CustomerId",
+                table: "Reservations",
+                column: "CustomerId",
+                principalTable: "Customers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
