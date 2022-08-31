52c5c15ca5b869f310790d180050a5e9fc67e30f
Flávio Tassan <ftassan@outlook.com>
2022-08-31 13:13:52 -0300

    Add RateLimitConfig.cs

---
+++ src/Services/CancunHotel.Services.API/Configurations/RateLimitConfig.cs
@@ -0,0 +1,14 @@
+﻿using AspNetCoreRateLimit;
+
+namespace CancunHotel.Services.API.Configurations;
+
+public static class RateLimitConfig
+{
+    public static void AddRateLimitConfiguration(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<ClientRateLimitOptions>(configuration.GetSection("ClientRateLimiting"));
+        services.Configure<ClientRateLimitPolicies>(configuration.GetSection("ClientRateLimitPolicies"));
+        services.AddInMemoryRateLimiting();
+        services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+    }
+}
