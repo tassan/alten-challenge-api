42c6457a7e7b661bf9d6a9eac3129302fb807b2e
Flávio Tassan <ftassan@outlook.com>
2022-08-31 17:04:39 -0300

    Add ResponseCompressionConfig.cs

---
+++ src/Services/CancunHotel.Services.API/Configurations/ResponseCompressionConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.IO.Compression;
+using Microsoft.AspNetCore.ResponseCompression;
+
+namespace CancunHotel.Services.API.Configurations;
+
+public static class ResponseCompressionConfig
+{
+    public static void AddResponseCompressionConfiguration(this IServiceCollection services)
+    {
+        services.Configure<GzipCompressionProviderOptions>(options => options.Level = CompressionLevel.Optimal);
+        services.AddResponseCompression(options =>
+        {
+            options.Providers.Add<GzipCompressionProvider>();
+        });
+    }
+}
