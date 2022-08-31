bc00552573f6d70cf67c783991885298ea363566
Flávio Tassan <ftassan@outlook.com>
2022-08-31 19:02:58 -0300

    Add HealthChecksConfig.cs

---
+++ src/Services/CancunHotel.Services.API/Configurations/HealthChecksConfig.cs
@@ -0,0 +1,20 @@
+﻿namespace CancunHotel.Services.API.Configurations;
+
+public static class HealthChecksConfig
+{
+    public static void AddHealthChecksConfiguration(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddHealthChecks()
+            .AddNpgSql(configuration.GetConnectionString("DefaultConnection"))
+            .AddApplicationInsightsPublisher();
+
+        services.AddHealthChecksUI(opt =>
+        {
+            opt.SetEvaluationTimeInSeconds(60);
+            opt.MaximumHistoryEntriesPerEndpoint(60);
+            opt.SetApiMaxActiveRequests(1);
+            opt.AddHealthCheckEndpoint("Cancun Hotel API", "/healthz");
+        })
+            .AddInMemoryStorage();
+    }
+}
