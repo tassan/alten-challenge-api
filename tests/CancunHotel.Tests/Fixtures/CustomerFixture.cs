137a547a0b8f74315999d461e162dff481f97d0b
Flávio Tassan <ftassan@outlook.com>
2022-08-31 01:16:57 -0300

    - Add functions to create Customer and CustomerViewModel collections - Create Invalid CustomerViewModel

--- tests/CancunHotel.Tests/Fixtures/CustomerFixture.cs
+++ tests/CancunHotel.Tests/Fixtures/CustomerFixture.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Bogus;
 using CancunHotel.Application.ViewModels;
 using CancunHotel.Domain.Entities;
@@ -25,6 +28,38 @@ public class CustomerFixture : IDisposable
             .RuleFor(c => c.Email, f => f.Person.Email)
             .RuleFor(c => c.BirthDate, f => f.Person.DateOfBirth)
             .Generate();
+    }
+    
+    public static CustomerViewModel CreateInvalidCustomerViewModel()
+    {
+        return new Faker<CustomerViewModel>()
+            .RuleFor(c => c.FirstName, f => f.Person.FirstName)
+            .RuleFor(c => c.LastName, f => f.Person.LastName)
+            .RuleFor(c => c.Email, f => f.Person.Email)
+            .RuleFor(c => c.BirthDate, f => DateTime.Now)
+            .Generate();
+    }
+    
+    public static async Task<IEnumerable<Customer>> CreateCustomers()
+    {
+        var customer = new Faker<Customer>()
+            .RuleFor(c => c.FirstName, f => f.Person.FirstName)
+            .RuleFor(c => c.LastName, f => f.Person.LastName)
+            .RuleFor(c => c.Email, f => f.Person.Email)
+            .RuleFor(c => c.BirthDate, f => f.Person.DateOfBirth);
+        
+        return await Task.FromResult(customer.Generate(10));
+    }
+    
+    public static async Task<IEnumerable<CustomerViewModel>> CreateCustomerViewModels()
+    {
+        var customerViewModel = new Faker<CustomerViewModel>()
+            .RuleFor(c => c.FirstName, f => f.Person.FirstName)
+            .RuleFor(c => c.LastName, f => f.Person.LastName)
+            .RuleFor(c => c.Email, f => f.Person.Email)
+            .RuleFor(c => c.BirthDate, f => f.Person.DateOfBirth);
+        
+        return await Task.FromResult(customerViewModel.Generate(10));
     }
 
     public void Dispose()
